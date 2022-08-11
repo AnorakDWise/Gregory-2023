d09cb658c643ad4a93864fc44fe140c2318ebff4
AnorakDWise <gregory442005@gmail.com>
2022-08-11 21:52:18 +0200

    init 2

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,3 +1,10 @@
+import home from "./components/home";
+import navbar from "./components/navbar";
+import about from "./components/about";
+import projects from "./components/projects";
+import skills from "./components/skills";
+import contact from "./components/contact";
+
 function App() {
   return (
     <main>
