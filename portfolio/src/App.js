60cb1e2809e3317bba2349fdf7f27554de98dd6d
AnorakDWise <gregory442005@gmail.com>
2022-08-11 20:15:45 +0200

    init

---
+++ portfolio/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <main>
+      <home />
+      <navbar />
+      <about />
+      <projects />
+      <skills />
+      <contact />
+    </main>
+  );
+}
+
+export default App;
